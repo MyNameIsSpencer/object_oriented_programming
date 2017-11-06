3fe5d00eeee900f6739e9a52394f3065ca2a6d97
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-06 16:28:25 -0500

    player done

--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   # gold_coins, health points, lives
+
   def initialize
     @gold_coins = 0
     @health_points = 100
@@ -29,6 +30,22 @@ class Player
     @lives
   end
 
+  def do_battle(damage)
+    @health_points -= damage
+    if @health_points <= 0
+      @lives -= 1
+      if @lives <= 0
+        restart
+      end
+      @health_points = 100
+    end
+  end
+
+  def restart
+    @gold_coins = 0
+    @health_points = 100
+    @lives = 5
+  end
 
 end
 
@@ -42,3 +59,6 @@ end
 puts "You have #{player1.health} health"
 puts "You have #{player1.gold} gold"
 puts "You have #{player1.lives} lives"
+player1.do_battle(110)
+puts "You have #{player1.lives} lives"
+puts "You have #{player1.health} health"
