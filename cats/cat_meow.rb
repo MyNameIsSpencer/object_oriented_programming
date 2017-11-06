9f0f02e461dffaef3a1670dfb2039822c7f570fc
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-06 15:23:34 -0500

    cats is done

--- cats/cat_meow.rb
+++ cats/cat_meow.rb
@@ -2,19 +2,27 @@ class Cat
   def initialize(name)
     @name = name
     @preferred_food = 0
-    @meal_time = 0
+    @meal_time = nil
   end
 
-  def time_to_eat(hour)
-    if hour = 0
-        hour = '12AM'
-      elsif hour >= 13 && hour < 24
-        hour -= 12 + 'PM'
-      elsif hour = 1 && hour <= 12
-        hour = hour + 'AM'
-        return hour
+  def eats_at(hour)
+    if hour <= 11
+      if hour == 0
+        hour = 12
+      end
+  @meal_time = hour.to_s + 'AM'
+  elsif hour >= 12 && hour < 24
+      if hour >= 13
+        hour -= 12
+      end
+    @meal_time = hour.to_s + "PM"
+    else
+      puts "INVALID NUMBER"
     end
-    @meal_time = hour
+  end
+
+  def meow
+    puts "Hi! Meow name is #{@name} and I eat #{@preferred_food} at #{@meal_time}"
   end
 
   def name
@@ -42,7 +50,7 @@ end
 
 
 
-puts '=======NEW CAT======='
+puts '=======STANKY======='
 
 stanky = Cat.new('Stanky')
 puts stanky.name
@@ -51,15 +59,15 @@ puts "What is #{stanky.name}'s favourite food"
 food = gets.chomp.to_s
 
 stanky.preferred_food(food)
-
 puts "#{stanky.name}'s preferred food is #{stanky.reader_food}."
 
-stanky.time_to_eat(15)
-puts stanky.meal_time
+stanky.eats_at(20)
+puts stanky.reader_meal_time
+
+stanky.meow
 
 
-
-puts '=======NEW CAT======='
+puts '=======SPANKY======='
 
 spanky = Cat.new('Spanky')
 puts spanky.name
@@ -70,3 +78,8 @@ food = gets.chomp.to_s
 spanky.preferred_food(food)
 
 puts "#{spanky.name}'s preferred food is #{spanky.reader_food}."
+
+spanky.eats_at(7)
+puts spanky.reader_meal_time
+
+spanky.meow
